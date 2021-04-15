3031747d403fa0402ba920129bd2031a0a605676
Dima <warreaver123@gmail.com>
2021-04-15 16:38:17 +0300

    before slider

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+    function toggleSlide (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.services__item').eq(i).toggleClass('services__item_active');
+                $('.services__up').eq(i).toggleClass('services__up_active');
+                $('.services__down').eq(i).toggleClass('services__down_active');
+            });
+        });
+    };
+    toggleSlide('.services__up');
+
+    $('.feedback__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+    });
+
+})
