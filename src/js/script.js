4a97a5f64bfd4eb146820615babefd53a9fc3172
Dima <warreaver123@gmail.com>
2021-04-16 15:45:53 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,37 @@ jQuery(document).ready(function($) {
     };
     toggleSlide('.services__up');
 
-    
+    $('.feedback__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-down" aria-hidden="true"></i></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-up" aria-hidden="true"></i></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
 
+              }
+            },
+          ]
+    });
+    
+    $('.team__slider').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+        arrows:false,
+        slidesToScroll: 1
+    });
+    
+    $('.team__slider').on('click',function(){
+        $(this).slick('slickNext');
+        $(this).slick('slickPrew');
+      });
 })
